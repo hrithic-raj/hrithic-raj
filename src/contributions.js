282a9129676dff325b05190495b3d617e27ade2a
Hrithic <hrithicraj730@gmail.com>
2026-01-01 13:04:07 +0530

    unneccesory codes removed

--- src/contributions.js
+++ src/contributions.js
@@ -1,31 +1,3 @@
-// const { writeFileSync } = require("node:fs");
-
-// const scrapeContributions = async () => {
-// 	const html = await (
-// 		await fetch("https://github.com/users/hrithic-raj/contributions")
-// 	).text();
-// 	const dateRegex = /<tool-tip.*<\/tool-tip>/gm;
-
-// 	const data = html.match(dateRegex).map((date) => {
-// 		const dataRegex =
-// 			/(?<=tool-tip.*contribution-day-component-)(?<row>\d+)-(?<col>\d+)(?:\n|.)*?(?<count>\d+|No)(?= contribution)/gm;
-
-// 		const extractedData = dataRegex.exec(date).groups;
-// 		return [
-// 			Number.parseInt(extractedData.row),
-// 			Number.parseInt(extractedData.col),
-// 			Number.parseInt(extractedData.count === "No" ? 0 : extractedData.count),
-// 		];
-// 	});
-// 	return data;
-// };
-
-// (async () => {
-// 	const contributions = await scrapeContributions();
-// 	writeFileSync("contributions.json", JSON.stringify(contributions));
-// })();
-
-
 const { writeFileSync } = require("node:fs");
 const path = require("node:path");
 
